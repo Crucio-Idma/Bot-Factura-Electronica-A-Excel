b5da6dbbf83ad4ec5a45b03ffbbe1b68be1fee01
Crucio <manettealberto@outlook.com>
2019-09-28 08:54:14 -0600

    Se crea una persona emisor y una persona receptor en a factura
    Se crean metodos para obtener los datos sin importar la clase persona

--- CajaDeHerramientas/factura.py
+++ CajaDeHerramientas/factura.py
@@ -3,16 +3,45 @@ Created on 28 sep. 2019
 
 @author: Crucio
 '''
-from PartesDeLaFactura import import persona 
+from PartesDeLaFactura import * 
+from CajaDeHerramientas.PartesDeLaFactura.persona import persona
 
 class Factura:
     
-    def __init__(self , pemisor, pidEmisor, preceptor, pidreceptor , pTotalImpuesto, pTotalVentaNeta, pTotalComprobante):
+    def __init__(self , pemisor ,ptipoIDEmisor ,pidEmisor, preceptor,pTidoIdReceptor, pidreceptor , pTotalImpuesto, pTotalVentaNeta, pTotalComprobante):
         
-        self.emisor = pemisor
         self.receptor = preceptor
         self.TotalImpuesto = pTotalImpuesto
         self.TotalVentaNeta= pTotalVentaNeta
         self.TotalComprobante = pTotalComprobante
     
+        self.emisor = persona( pNombre =pemisor, pTipoIdentificacion = ptipoIDEmisor ,pIdentificacion = pidEmisor )
+        self.receptor = persona( pNombre = preceptor, pTipoIdentificacion = pTidoIdReceptor ,pIdentificacion = pidreceptor )
         
+    
+    def obtenerNombreEmisor(self):       
+        
+        return self.emisor.Nombre
+    
+    def obtenerTipoIDEmisor(self):
+        
+        return self.emisor.TipoIdentificacion
+    
+    def obtenerIDEmisor(self):
+        
+        return self.emisor.Identificacion
+    
+    def obtenerNombreReceptor(self):       
+        
+        return self.receptor.Nombre
+    
+    def obtenerTipoIDReceptor(self):
+        
+        return self.receptor.TipoIdentificacion
+    
+    def obtenerIDReceptor(self):
+        
+        return self.receptor.Identificacion
+    
+    
+    
