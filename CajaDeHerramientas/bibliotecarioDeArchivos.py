f1e386e776d97daf4129bc660b5689d55b336c1e
Crucio <manettealberto@outlook.com>
2019-09-28 05:29:13 -0600

    Se carga el archivo xml de configuraciones

---
+++ CajaDeHerramientas/bibliotecarioDeArchivos.py
@@ -0,0 +1,28 @@
+'''
+Created on 28 sep. 2019
+
+@author: Crucio
+
+
+
+'''
+
+"""
+    Se Genera una lista con las direcciones de los archivos
+    @param pUrlDeLaCarpetaDeEntrada: String con la dirección de la carpeta raíz 
+    @return Retorna una lista con todos las facturas virtuales
+
+"""
+def buscarFacturasEnLasCarpetas(pUrlDeLaCarpetaDeEntrada):
+
+    return [True]
+
+"""
+    Se revisa si la url es relativa o absoluta
+    @param pUrl: String con la url
+    @return: Retorna True si la url es absoluta, False en caso de que sea relativa
+"""    
+def esUrlAbsoluta(pUrl):
+    return True
+    
+    
--- main.py
+++ main.py
@@ -1,2 +1,30 @@
+"""
+Created on 28 sep. 2019
+
+@author: Crucio
+""" 
  
- 
+#===================================================================================
+
+from  xml.dom import minidom
+from CajaDeHerramientas import bibliotecarioDeArchivos
+
+#Variables de inicio================================================================
+
+urlDelArchivoDeConfiguraciones = "configuraciones/manejoDeArchivos.xml"
+
+#===================================================================================
+
+#Se carga el archivo XML============================================================
+try:
+    _ArchivoConfig = minidom.parse(urlDelArchivoDeConfiguraciones)
+    
+except Exception as error:
+    print("No se pudo cargar el archivo de configuraciones\n")
+    print("El error ocurrido a sido:\n" + str(error))
+
+
+
+_UrlInputDeArchivos =  _ArchivoConfig.getElementsByTagName("urlCarpetaDeEntrada")[0].firstChild.data
+
+
